8d84648d81cc2f5528c9762b4251023d81dfe6a3
DESKTOP-8TCPMCB\Liem Phan <minhliemphp@gmail.com>
2017-10-02 07:51:37 +0700

    update 02-10

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-export FlashData{
+export class FlashData{
     static data = {};
 
     static get(key){
@@ -16,5 +16,15 @@ export FlashData{
 
     static set(key, value){
         this.data[key] = value;
+    }
+}
+
+export class Pagination extends Component{
+    getUrl(page =1){
+        let url = `/${this.props.to}?page=${page}`;
+        if(this.props.keyword){
+            url += `&search=${this.props.keyword}`;
+        }
+        return url;
     }
 }
--- src/users/list.js
+++ src/users/list.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import UserModel from '../models/users';
+import {Link} from 'react-router-dom';
 
-class UserList extends Component{
-    constructor(props){
+class UserList extends Component {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -10,12 +11,12 @@ class UserList extends Component{
         }
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.getUser();
     }
 
-    getUser(){
-        UserModel.getUsers().then( result => {
+    getUser() {
+        UserModel.getUsers().then(result => {
             this.setState({
                 users: result.data
             })
@@ -25,33 +26,57 @@ class UserList extends Component{
     render() {
         return (
             <div className="user-list">
+                <h4 className="page-header">Danh Sách người dùng</h4>
+                <div className="control-bar mb-3">
+                    <Link to="/users/create" className="btn btn-primary px-4"><i className="fa fa-plus"></i> Tạo
+                        mới</Link>&nbsp;
+                    <Link to="/users/create" className="btn btn-outline-dark px-4"><i className="fa fa-refresh"></i>
+                        Refresh</Link>&nbsp;
+                    <form style={{'display': 'inline-block'}} className="form-inline">
+                        <input
+                            type="text"
+                            name="keyword"
+                            className="form-control"
+                            placeholder="Tìm kiếm"
+                        />
+                    </form>
+                </div>
                 <table className="table table-responsive">
                     <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Chức năng</th>
-                        </tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Chức năng</th>
+                    </tr>
                     </thead>
                     <tbody>
                     {
                         this.state.users !== null && this.state.users.data.length > 0 ?
                             this.state.users.data.map((value, key) => (
-                                <tr key={key+1}>
+                                <tr key={key + 1}>
                                     <td>{value.id}</td>
                                     <td>{value.name}</td>
                                     <td>{value.email}</td>
                                     <td>Xóa | Sửa</td>
                                 </tr>
                             ))
-                        :
+                            :
                             <tr>
                                 <td colSpan="4">Không có dữ liệu</td>
                             </tr>
                     }
                     </tbody>
                 </table>
+                <nav aria-label="Page navigation example" className="d-flex justify-content-center ">
+                    <ul className="pagination">
+                        <li className="page-item"><a className="page-link" href="#">Trang đầu</a></li>
+                        <li className="page-item"><a className="page-link" href="#">1</a></li>
+                        <li className="page-item"><a className="page-link" href="#">2</a></li>
+                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                        <li className="page-item"><a className="page-link" href="#">Trang cuối</a></li>
+                    </ul>
+                </nav>
             </div>
         );
     }
